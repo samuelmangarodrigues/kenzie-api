38fa519ef29946b878641b7cfb059dc01c7aaecd
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-10-13 08:37:10 -0300

    deploy

--- kenzie_todo/settings.py
+++ kenzie_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-l^vmt4=(dx3zakz28!&hv@5@-y7zr7z@tvifu0o6hl9d#=4s%%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://kenzie-test-todo.herokuapp.com/']
 
 
 # Application definition
@@ -123,3 +124,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+django_heroku.settings(locals())
